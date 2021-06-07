968b97fb59df50f0bc549bf80ebdd34a3aed4d01
thomkorteweg <>
2021-06-07 14:11:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Wat is je naam?")
+
+
+woordenlijst = ["Galgje","jesseper","ginger","blackdead","paashaas"]
+print (woordenlijst)
+print = random.choice
---
+++ woordenlijst.py
@@ -0,0 +1,1 @@
+woordenlijst = "Galgje"
